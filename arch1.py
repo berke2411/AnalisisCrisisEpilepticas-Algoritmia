268d3030fc2dcb4d23e29d9ec823acab90a4c6f0
berke2411 <nachoberk@gmail.com>
2026-04-30 15:59:44 -0300

    termine la 1ra parte

--- arch1.py
+++ arch1.py
@@ -3,15 +3,16 @@ import numpy as np
 from pyedflib import highlevel
 
 # 1. Definir los parámetros extraídos del archivo summary.txt del paciente
-edf_file = 'chb01_03.edf'  # Archivo de ejemplo
-start_sec = 2996           # Seizure Start Time en segundos
-end_sec = 3036             # Seizure End Time en segundos
+edf_file = './archivos/chb20_12.edf'  # Archivo de ejemplo
+start_sec = 94           # Seizure Start Time en segundos
+end_sec = 123             # Seizure End Time en segundos
 
 # 2. Cargar las señales y metadatos usando highlevel
 signals, signal_headers, header = highlevel.read_edf(edf_file)
 
 # Extraemos la frecuencia de muestreo (fs) del primer canal (suele ser igual para todos)
-fs = int(signal_headers['sample_rate'])
+#print(signal_headers)
+fs = 256
 
 # 3. Conversión de tiempos (Regla de 3 simple: 1 Hz = 1 muestra por segundo)
 # Calculamos en qué "muestra" exacta empieza y termina la crisis
@@ -23,9 +24,21 @@ window_samples = 120 * fs
 
 # 4. Segmentación - Escenario 1 (Por Bloques)
 # Señal total = 2 min antes + crisis + 2 min después
-before_seizure = signals[:, start_sample - window_samples : start_sample]
+# Usamos max() para evitar índices negativos si la crisis empieza antes de los 2 min
+
+# Obtenemos la cantidad total de muestras que tiene el archivo (columnas de la matriz)
+total_samples = signals.shape[1]
+
+
+# Límite inferior: usamos max() para no tener índices negativos
+inicio_before = max(0, start_sample - window_samples)
+
+# Límite superior: usamos min() para no pasarnos del largo total del archivo
+fin_after = min(total_samples, end_sample + window_samples)
+
+before_seizure = signals[:, inicio_before : start_sample]
 seizure = signals[:, start_sample : end_sample]
-after_seizure = signals[:, end_sample : end_sample + window_samples]
+after_seizure = signals[:, end_sample : fin_after]
 
 # 5. Centrado de señales
 # Fundamental: Restarle la media a cada segmento por canal para que se centre.
